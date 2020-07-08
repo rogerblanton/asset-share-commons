3942bc4e47d57f6270cbd020798d667e222b358b
David Gonzalez <davidjgonzalez@gmail.com>
2020-07-08 12:02:03 -0400

    $PWD

--- release.sh
+++ release.sh
@@ -5,8 +5,8 @@ mvn clean package -Dbnd.baseline.skip=true
 git commit -m "$1 release"
 git push
 
-mvn deploy:deploy-file -Durl=file://./staging-maven-repository/ -Dfile=./ui.content/target/com.adobe.aem.demo.demo-utils.ui.content-$1.zip -DgroupId=com.adobe.aem.demo -DartifactId=com.adobe.aem.demo.demo-utils.ui.content -Dpackaging=zip -Dversion=$1
-mvn deploy:deploy-file -Durl=file://./staging-maven-repository/ -Dfile=./ui.apps/target/com.adobe.aem.demo.demo-utils.ui.apps-$1.zip -DgroupId=com.adobe.aem.demo -DartifactId=com.adobe.aem.demo.demo-utils.ui.apps -Dpackaging=zip -Dversion=$1
+mvn deploy:deploy-file -Durl=file://$PWD/staging-maven-repository/ -Dfile=$PWD/ui.content/target/com.adobe.aem.demo.demo-utils.ui.content-$1.zip -DgroupId=com.adobe.aem.demo -DartifactId=com.adobe.aem.demo.demo-utils.ui.content -Dpackaging=zip -Dversion=$1
+mvn deploy:deploy-file -Durl=file://$PWD/staging-maven-repository/ -Dfile=$PWD/ui.apps/target/com.adobe.aem.demo.demo-utils.ui.apps-$1.zip -DgroupId=com.adobe.aem.demo -DartifactId=com.adobe.aem.demo.demo-utils.ui.apps -Dpackaging=zip -Dversion=$1
 
 mvn versions:set -DnewVersion=$2
 git commit -m "$3 release"
